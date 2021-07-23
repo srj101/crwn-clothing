98f4193bd42b8dd89140158da2127a778df6454f
SR Joy <salim15-3117@diu.edu.bd>
2021-07-23 19:20:06 +0600

    Created Project and added files for the homepage

---
+++ src/HomePage.component.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import './HomePage.styles.scss';
+
+export const HomePage = () => (
+    <div className="menu-container">
+        <div className="menu-item">
+            <div className="content">
+                <h3 className="title">
+                    JACKETS
+                </h3>
+                <p>SHOP NOW</p>
+            </div>
+        </div>
+        <div className="menu-item">
+            <div className="content">
+                <h3 className="title">
+                    HATS
+                </h3>
+                <p>SHOP NOW</p>
+            </div>
+        </div>
+        <div className="menu-item">
+            <div className="content">
+                <h3 className="title">
+                    SNEAKERS
+                </h3>
+                <p>SHOP NOW</p>
+            </div>
+        </div>
+        <div className="menu-item">
+            <div className="content">
+                <h3 className="title">
+                    WOMENS
+                </h3>
+                <p>SHOP NOW</p>
+            </div>
+        </div>
+        <div className="menu-item">
+            <div className="content">
+                <h3 className="title">
+                    MENS
+                </h3>
+                <p>SHOP NOW</p>
+            </div>
+        </div>
+    </div>
+)
